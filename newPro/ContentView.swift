ef974150ee2c34ab81d89e7b3af2975d2de7f4a4
harmander <harmander.singh.rai@gmail.com>
2021-01-24 09:17:02 -0800

    harmander,s commit

---
+++ newPro/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  newPro
+//
+//  Created by jimmy on 24/01/21.
+//  Copyright © 2021 jimmy. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
